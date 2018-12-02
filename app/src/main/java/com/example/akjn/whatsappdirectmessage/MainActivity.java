80ef7eca86ef51eaaea1c43cd0207a4ba5cac897
Mathan.S <capturesmk@gmail.com>
2018-12-02 17:04:52 +0530

    Added Menu to the application

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.akjn.whatsappdirectmessage;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
@@ -15,11 +17,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void contactOnWhatsApp(View v)
-    {
-        EditText phoneNumberField = (EditText)findViewById(R.id.inputField);
+    public void contactOnWhatsApp(View v) {
+        EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
         String phoneNumber = phoneNumberField.getText().toString();
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone="+phoneNumber));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
         startActivity(browserIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+
+            case R.id.darktheme:
+                // TODO Add Dark Theme
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
