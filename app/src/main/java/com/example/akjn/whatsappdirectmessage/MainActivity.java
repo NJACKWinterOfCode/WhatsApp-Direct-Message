f5d3fbf8ed1322bc4c0b40a295c9ed923f2ea841
Amartya Mondal <hayyoulistentome@gmail.com>
2018-12-05 00:45:21 +0530

    Added a simple verification for checking the validity of mobile number entered #12

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.akjn.whatsappdirectmessage;
 
 import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,11 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void contactOnWhatsApp(View v)
-    {
-        EditText phoneNumberField = (EditText)findViewById(R.id.inputField);
+    public void contactOnWhatsApp(View v) {
+        EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
         String phoneNumber = phoneNumberField.getText().toString();
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone="+phoneNumber));
-        startActivity(browserIntent);
+        int length = phoneNumber.length();
+        if (length == 12) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
+            startActivity(browserIntent);
+        }
+        else{
+            Toast.makeText(this, "Enter mobile number with proper country code",
+                    Toast.LENGTH_LONG).show();
+        }
     }
 }
