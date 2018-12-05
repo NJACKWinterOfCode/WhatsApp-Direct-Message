05dd88ed0d25b7e17e4b362135c6e0d28a83d1f8
Niveth Saran <31360464+nivethsaran@users.noreply.github.com>
2018-12-05 22:00:13 +0530

    Dark Theme

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -1,22 +1,66 @@
 package com.example.akjn.whatsappdirectmessage;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.view.View;
 import android.content.Intent;
 import android.net.Uri;
+import android.widget.Switch;
 
 import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
-
+Switch s;
+boolean isEnabled1=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+       // AppCompatDelegate.setDefaultNightMode(
+         //       AppCompatDelegate.MODE_NIGHT_YES);
+            Intent intent = getIntent();
+            boolean b1=intent.getBooleanExtra("state",false);
+            if(b1)
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+            }
+            else
+            {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        s=(Switch)findViewById(R.id.switch1);
+            
+        s.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+
+                    isEnabled1=true;
+                    restartApp();
+                }else
+                {
+                    isEnabled1=false;
+                restartApp();}
+            }
+        });
     }
 
+public void restartApp()
+{
+    Intent i=new Intent(getApplicationContext(),MainActivity.class);
+    i.putExtra("state",isEnabled1);
+    startActivity(i);
+    finish();
+
+}
     public void contactOnWhatsApp(View v)
     {
         EditText phoneNumberField = (EditText)findViewById(R.id.inputField);
