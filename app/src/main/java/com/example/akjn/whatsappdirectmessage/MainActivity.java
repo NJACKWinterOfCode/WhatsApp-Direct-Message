088eb022d0c8c73c9c478be9df784371fec3e5a1
somenath sarkar <somenath1435@gmail.com>
2018-12-20 01:39:14 +0530

    Removed help Activity

--- app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
+++ app/src/main/java/com/example/akjn/whatsappdirectmessage/MainActivity.java
@@ -29,6 +29,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.widget.AdapterView;
 import android.widget.Spinner;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
@@ -46,11 +47,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        if(AppCompatDelegate.getDefaultNightMode()==AppCompatDelegate.MODE_NIGHT_YES)
-        {
+        if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
             setTheme(R.style.DarkTheme);
-        }
-        else
+        } else
             setTheme(R.style.AppTheme);
 
         super.onCreate(savedInstanceState);
@@ -59,55 +58,52 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-        @Override
-        public boolean onCreateOptionsMenu(Menu menu)
-        {
-            MenuInflater inflater = getMenuInflater();
-            inflater.inflate(R.menu.action_menu, menu);
-            return true;
-        }
-    public void contactOnWhatsApp (View v){
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_menu, menu);
+        return true;
+    }
 
-            EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
+    public void contactOnWhatsApp(View v) {
 
-
-            //  CODE FOR COUNTRY CODE SPINNER
-
-            CountryCodePicker cpp = (CountryCodePicker) findViewById(R.id.cpp);
-            cpp.registerCarrierNumberEditText(phoneNumberField);
-            String phoneNumber = cpp.getFullNumber();
+        EditText phoneNumberField = (EditText) findViewById(R.id.inputField);
 
 
-            //Toast.makeText(MainActivity.this,phoneNumber,Toast.LENGTH_LONG).show();
+        //  CODE FOR COUNTRY CODE SPINNER
+
+        CountryCodePicker cpp = (CountryCodePicker) findViewById(R.id.cpp);
+        cpp.registerCarrierNumberEditText(phoneNumberField);
+        String phoneNumber = cpp.getFullNumber();
 
 
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
-            startActivity(browserIntent);
-        }
+        //Toast.makeText(MainActivity.this,phoneNumber,Toast.LENGTH_LONG).show();
+
+
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://api.whatsapp.com/send?phone=" + phoneNumber));
+        startActivity(browserIntent);
+    }
 
     //Creating the Actions for the menu items
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case R.id.help:
-                Intent intent = new Intent(MainActivity.this, help.class);
-                startActivity(intent);
-                break;
+
             case R.id.dark_mode:
                 //add code here
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
-                Intent i=new Intent(MainActivity.this,MainActivity.class);
+                Intent i = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(i);
                 finish();
-                return true;
+                break;
 
             case R.id.lightmode:
                 //add code here
                 AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
-                Intent ins=new Intent(MainActivity.this,MainActivity.class);
+                Intent ins = new Intent(MainActivity.this, MainActivity.class);
                 startActivity(ins);
                 finish();
-                return true;
+                break;
 
 
             default:
@@ -117,14 +113,14 @@ public class MainActivity extends AppCompatActivity {
     }
 
     //Function to save contact
-    public void SaveContact (View v) {
+    public void SaveContact(View v) {
 
         Intent intent = new Intent(ContactsContract.Intents.Insert.ACTION);
         intent.setType(ContactsContract.RawContacts.CONTENT_TYPE);
-        mPhoneNumber= (EditText) findViewById(R.id.inputField);
+        mPhoneNumber = (EditText) findViewById(R.id.inputField);
 
         CountryCodePicker cpp = (CountryCodePicker) findViewById(R.id.cpp);
-        String mNo= cpp.getFullNumberWithPlus() + mPhoneNumber.getText();
+        String mNo = cpp.getFullNumberWithPlus() + mPhoneNumber.getText();
         intent.putExtra(ContactsContract.Intents.Insert.PHONE, mNo);
 
         startActivity(intent);
@@ -134,7 +130,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Function to clear numbers
     public void clearNumber(View view) {
-        EditText hello = (EditText)findViewById(R.id.inputField);
+        EditText hello = (EditText) findViewById(R.id.inputField);
         hello.setText("");
     }
 }
